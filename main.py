6226045f55b75984c82b0a1d0d0dfe8540968385
Timur Erofeev <timur.erofeev06@gmail.com>
2024-03-24 18:55:45 +0300

    тестовый запуск черепашки

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from turtle import *
+screensize(600,600)
+# bgpic("elka.png")
+# hideturtle()
+shape("turtle")
+penup()
 
+# украшаем елку
+goto(80,170)
+dot(40,"red")
+# ваш код тут...
+
+# _
+mainloop()
